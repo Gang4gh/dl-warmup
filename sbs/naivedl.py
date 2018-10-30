dd35ff5c1329cd3fb02929c0df0cc295c2d02179
Gang Luo <36316266+gang4gh@users.noreply.github.com>
2018-10-30 01:38:45 -0700

    sbs naivedl, acc=0.6856

---
+++ sbs/naivedl.py
@@ -0,0 +1,99 @@
+import sys
+import os
+import random
+import collections
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Embedding, LSTM, Dense
+from sklearn.metrics import accuracy_score
+
+from vocab import Vocab
+from sbs_data import load_data
+
+def config_environment(model_dir):
+	# run multiple instances in one GPU
+	config = tf.ConfigProto()
+	config.gpu_options.allow_growth=True
+	tf.keras.backend.set_session(tf.Session(config=config))
+
+	if not os.path.exists(model_dir):
+		os.mkdir(model_dir)
+
+def prepare_dataset(vocab, batch_size, data):
+	if len(data) % batch_size != 0:
+		data = data[:-(len(data) % batch_size)]
+
+	def words_to_ids(text, max_word_count):
+		ids = [vocab.get_id_by_word(w) for w in text.split()[:max_word_count]]
+		return [vocab.token_pad_id] * (max_word_count - len(ids)) + ids
+
+	x0 = [words_to_ids(rec.query, 20) for rec in data]
+	x1 = [words_to_ids(rec.snippet1, 120) for rec in data]
+	x2 = [words_to_ids(rec.snippet2, 120) for rec in data]
+	y = tf.keras.utils.to_categorical([rec.label+1 for rec in data], 3)
+	return [x0, x1, x2], y
+
+def build_and_train_model(batch_size, model_dir, training_set, validation_set):
+	query_input = Input(shape=(20,), dtype='int32')
+	snippet1_input = Input(shape=(120,), dtype='int32')
+	snippet2_input = Input(shape=(120,), dtype='int32')
+
+	embedding = Embedding(input_dim=50000, output_dim=128)
+	query_embedding = embedding(query_input)
+	snippet1_embedding = embedding(snippet1_input)
+	snippet2_embedding = embedding(snippet2_input)
+
+	query_lstm = LSTM(128)
+	snippet_lstm = LSTM(128)
+	query_encoding = query_lstm(query_embedding)
+	snippet1_encoding = snippet_lstm(snippet1_embedding)
+	snippet2_encoding = snippet_lstm(snippet2_embedding)
+
+	all_encoding = keras.layers.concatenate([query_encoding, snippet1_encoding, snippet2_encoding])
+	X = Dense(256, activation='relu')(all_encoding)
+	X = Dense(256, activation='relu')(X)
+	X = Dense(256, activation='relu')(X)
+	y = Dense(3, activation='softmax')(X)
+
+	model = tf.keras.models.Model(inputs=[query_input, snippet1_input, snippet2_input], outputs=[y])
+	model.summary()
+
+	model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+	checkpoint_path = model_dir + '/cp.{epoch:02d}.ckpt'
+	cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, verbose=1)
+	es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=100)
+	tb_callback = tf.keras.callbacks.TensorBoard(log_dir='{0}/tb'.format(model_dir))
+
+	model.fit(*training_set, epochs=2, batch_size=batch_size, callbacks=[cp_callback, es_callback, tb_callback], validation_data=validation_set)
+	model.save(model_dir + '/model')
+
+	return model
+
+Config = collections.namedtuple('Config', 'data_path vocab_path model_dir batch_size')
+cfg = Config(
+	data_path = 'sbsdatacleaned20181008_flips_tokLower.tsv',
+	vocab_path = 'trainingdata.vocab',
+	model_dir = 'model_v1030',
+	batch_size = 512,
+	)
+
+if __name__ == '__main__':
+	config_environment(cfg.model_dir)
+	training_data, test_data = load_data(cfg.data_path) # data is shuffled by default
+
+	vocab = Vocab(cfg.vocab_path, 50000)
+	val_offset = len(training_data) // 10 # split 10% training data as validation data
+	random.Random(1016).shuffle(training_data)
+	training_set = prepare_dataset(vocab, cfg.batch_size, training_data[:-val_offset])
+	validation_set = prepare_dataset(vocab, cfg.batch_size, training_data[-val_offset:])
+	test_set = prepare_dataset(vocab, cfg.batch_size, test_data)
+	print('training/validation/test set sizes : %d/%d/%d' % (len(training_set[0][0]), len(validation_set[0][0]), len(test_set[0][0])))
+
+	model = build_and_train_model(cfg.batch_size, cfg.model_dir, training_set, validation_set)
+	pred = np.argmax(model.predict(test_set[0], batch_size=cfg.batch_size), -1)
+
+	acc = accuracy_score(np.argmax(test_set[1], -1), pred)
+	print('accuracy on test_set :', acc)
+
---
+++ sbs/vocab.py
@@ -0,0 +1,106 @@
+"""Vocab class to convert word to id and vice versa"""
+
+import glob
+import random
+import logging
+
+import data_generic as dg
+
+
+class Vocab():
+	"""Vocabulary class that supports 'index pointer'"""
+	def __init__(self, vocab_file_path, vocab_size_cap, index_vocab_size=0):
+		""" Args:
+				vocab_file_path: path of .vocab file with tokens ordered by frequencies
+				vocab_size_cap: max vocab size including special tokens
+				index_vocab_size: max input length as index vocab size, 0 to disable pointer
+		"""
+		self._word_to_id = {}
+		self._id_to_word = {}
+		self._index_vocab_size = index_vocab_size
+
+		self.token_pad_id = self._add_word(dg.TOKEN_PAD)
+		self.token_oov_id = self._add_word(dg.TOKEN_OOV)
+		self.token_start_id = self._add_word(dg.TOKEN_START)
+		self.token_end_id = self._add_word(dg.TOKEN_END)
+		self.token_eos_id = self._add_word(dg.TOKEN_EOS)
+		SPECIAL_TOKEN_COUNT = len(self._word_to_id)
+
+		with open(vocab_file_path) as f:
+			for line in f:
+				parts = line.split()
+				if len(parts) != 2:
+					raise ValueError('Invalid vocab file, line = [%s]' % line)
+				self._add_word(parts[0])
+				if len(self._word_to_id) >= vocab_size_cap + SPECIAL_TOKEN_COUNT: # special tokens added before
+					break
+
+		self._static_vocab_size = len(self._word_to_id)
+		logging.info('load %d tokens (including %d special tokens, %d index tokens) into vocab.',
+			self.get_vocab_size(), SPECIAL_TOKEN_COUNT, self._index_vocab_size)
+
+	def _add_word(self, word):
+		if word in self._word_to_id:
+			raise ValueError('Duplicated word: %s' % word)
+		word_id = len(self._word_to_id)
+		self._word_to_id[word] = word_id
+		self._id_to_word[word_id] = word
+		return word_id
+
+	def get_vocab_size(self):
+		return self._static_vocab_size + self._index_vocab_size
+
+	def get_id_by_word(self, word, reference=None):
+		if word in self._word_to_id:
+			return self._word_to_id[word]
+		elif self._index_vocab_size == 0 or reference is None:
+			return self.token_oov_id
+		else:
+			try:
+				rid = reference.index(word)
+				if rid >= self._index_vocab_size:
+					logging.warning('rid(%d) is out of index_vocab_size(%d)', rid, self._index_vocab_size)
+					return self.token_oov_id
+				else:
+					return self._static_vocab_size + rid
+			except ValueError:
+				return self.token_oov_id
+
+	def get_word_by_id(self, id, reference=None, markup=False):
+		if id >= 0 and id < self._static_vocab_size:
+			return self._id_to_word[id]
+		elif id >= self._static_vocab_size and id < self.get_vocab_size():
+			rid = id - self._static_vocab_size
+			word = reference[rid] if rid < len(reference) else dg.TOKEN_OOV
+			if markup:
+				return '__%s_(%d/%d)' % (word, rid, self.get_id_by_word(word))
+			else:
+				return word
+		else:
+			raise ValueError('Invalid vocab id: %s' % id)
+
+	def parse_article(self, article_line, sent_id):
+		_, summary, article = article_line.split('\t')
+		if sent_id is not None:
+			summary = ' '.join(summary.split(dg.TOKEN_EOS_SPACES)[sent_id:sent_id+1])
+		article_tokens = [word for word in article.split()]
+		summary_tokens = [word for word in summary.split()]
+		article_ids = [self.get_id_by_word(word, article_tokens) for word in article_tokens]
+		summary_ids = [self.get_id_by_word(word, article_tokens) for word in summary_tokens]
+		return article_ids, article_tokens, article, summary_ids, summary_tokens, summary
+
+
+def check_vocab_stats(article_path, vocab_path, vocab_cap = 50000, vocab_index_size = 120):
+	vocab = Vocab(vocab_path, vocab_cap, vocab_index_size)
+	line_count, total_word_count, index_word_count, oov_word_count = 0, 0, 0, 0
+	for line in open(article_path):
+		art_ids, _, _, summary_ids, _, _ = vocab.parse_article(line.strip())
+		line_count += 1
+		total_word_count += len(summary_ids)
+		index_word_count += sum(1 for wid in summary_ids if wid >= vocab_cap + 4)
+		oov_word_count += sum(1 for wid in summary_ids if wid == vocab.token_oov_id)
+	print('read %d lines' % line_count)
+	print('total_word_count = %d, index_word_count = %d, oov_word_count = %d' % (total_word_count, index_word_count, oov_word_count))
+	print('index/total = %f, oov/total = %f' % (index_word_count / total_word_count, oov_word_count / total_word_count))
+
+#check_vocab_stats('training.articles', 'training.vocab', 10000)
