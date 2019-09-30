d70204b5ef173f0b5d56f0386338b614f7f37621
Gang Luo <36316266+gang4gh@users.noreply.github.com>
2019-09-30 15:44:48 -0700

    fix a memory issue in build-vocab code

--- dcap/dtitle_data/process_dtitle_data.py
+++ dcap/dtitle_data/process_dtitle_data.py
@@ -5,13 +5,18 @@ import re
 import time
 
 
-def dtitle_reader(dtitle_file):
+def dtitle_reader(dtitle_file, log_per_n_step=None):
+	lcount = 0
 	for l in open(dtitle_file, 'r', encoding='utf8'):
 		inputs = l.strip().split('\t')
 		if len(inputs) != 3:
 			print('invalid input, len(inputs)!=3, {}'.format(inputs[0]), file=sys.stderr)
 			continue
 		yield inputs
+		if log_per_n_step:
+			lcount += 1
+			if lcount % log_per_n_step == 0:
+				print('read {}k examples from {}'.format(lcount//1024, dtitle_file), file=sys.stderr)
 
 
 def preprocess_raw_input(input_file):
@@ -30,14 +35,17 @@ def preprocess_raw_input(input_file):
 			continue
 
 
-def build_vocab(input_file, vocab_file):
+def build_vocab(input_file, vocab_file_prefix, target_vocab_size):
 	import tensorflow_datasets as tfds
-	print('{}: start of building a subwords tokenizer.'.format(time.asctime()))
+	target_vocab_file = '{}-{}'.format(vocab_file_prefix, target_vocab_size)
+	print('{}: start to build a subwords tokenizer({}).'.format(time.asctime(), target_vocab_file))
 	tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-		(s.encode() for inputs in dtitle_reader(input_file) for s in inputs[1:]),
-		target_vocab_size=2**13)
-	tokenizer.save_to_file(vocab_file)
-	print('{}: prepare tokenizers and save to "{}".'.format(time.asctime(), vocab_file))
+		(s.encode() for inputs in dtitle_reader(input_file, 100*1024) for s in inputs[1:]),
+		target_vocab_size=int(target_vocab_size),
+		max_subword_length=16)
+	tokenizer.save_to_file(target_vocab_file)
+	print('{}: the subwords tokenizer({}) is ready.'.format(time.asctime(), target_vocab_file))
+
 
 if __name__ == '__main__':
 	cmd = sys.argv[1] if len(sys.argv) > 1 else None
