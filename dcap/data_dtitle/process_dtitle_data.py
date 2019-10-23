556d6cd0e788ca08e594cb21ab2018b41059ba92
Gang Luo <36316266+gang4gh@users.noreply.github.com>
2019-10-22 18:11:23 -0700

    conusme .gz files in data_dtitle

--- dcap/data_dtitle/process_dtitle_data.py
+++ dcap/data_dtitle/process_dtitle_data.py
@@ -3,13 +3,19 @@
 import sys
 import re
 import time
+import gzip
 from absl import app
 from absl import flags
 
 def dtitle_reader(dtitle_file, log_per_n_step=None):
 	lcount = 0
-	for l in open(dtitle_file, 'r', encoding='utf8'):
-		inputs = l.strip().split('\t')
+
+	is_gz_file = dtitle_file.endswith('.gz')
+	fin = gzip.open(dtitle_file) if is_gz_file else open(dtitle_file, encoding='utf8')
+
+	for l in fin:
+		inputs = l.decode('utf8') if is_gz_file else l
+		inputs = inputs.strip().split('\t')
 		if len(inputs) != 3:
 			print('invalid input, len(inputs)!=3, {}'.format(inputs[0]), file=sys.stderr)
 			continue
@@ -29,10 +35,10 @@ def preprocess_raw_input(FLAGS):
 		inputs[1] = re.sub(r'#N#|#R#|#TAB#', ' ', inputs[1])
 		inputs[2] = re.sub(r'</html>.*', '</html>', inputs[2], flags=re.I)
 
-		if FLAGS.filter_title:
+		if FLAGS.remove_title:
 			inputs[2] = re.sub(r'<title.*?</title>', ' ', inputs[2], flags=re.I)
 
-		if FLAGS.filter_head:
+		if FLAGS.remove_head:
 			inputs[2] = re.sub(r'<head.*?</head>', ' ', inputs[2], flags=re.I)
 
 		m = re.match(r'.{,3072}[^\w&</]', inputs[2])
@@ -43,10 +49,10 @@ def preprocess_raw_input(FLAGS):
 			continue
 
 		inputs = [re.sub(r' +', ' ', s).strip().lower() for s in inputs]
-		if FLAGS.ignore_noexactmatch and inputs[1] not in inputs[2]:
+		if FLAGS.check_exactmatch and inputs[1] not in inputs[2]:
 			continue
 
-		if FLAGS.ignore_nofuzzymatch:
+		if FLAGS.check_fuzzymatch:
 			htmlbody_lower = inputs[2].lower()
 			title_tokens = [w.lower() for w in re.split(r'\W+', inputs[1]) if w]
 			if any(token not in htmlbody_lower for token in title_tokens):
@@ -59,9 +65,8 @@ def preprocess_raw_input(FLAGS):
 
 def build_vocab(FLAGS):
 	import tensorflow_datasets as tfds
-	target_vocab_file = '{}-{}-{}-{}g'.format(FLAGS.vocab_file_prefix, FLAGS.target_vocab_size, FLAGS.max_subword_length, FLAGS.max_corpus_chars)
-	target_vocab_file_short = '{}-{}'.format(FLAGS.vocab_file_prefix, FLAGS.target_vocab_size)
-	print('{}: start to build a subwords tokenizer({}).'.format(time.asctime(), target_vocab_file))
+	target_vocab_file = '{}-{}'.format(FLAGS.vocab_file_prefix, FLAGS.target_vocab_size)
+	print('{}: start to build a subwords tokenizer({}) with max_subword_length={}, max_corpus_chars={}g.'.format(time.asctime(), target_vocab_file, FLAGS.max_subword_length, FLAGS.max_corpus_chars))
 	corpus = (s.encode() for inputs in dtitle_reader(FLAGS.input_file, 100*1024) for s in inputs[1:])
 	tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(corpus,
 			target_vocab_size = FLAGS.target_vocab_size,
@@ -69,7 +74,6 @@ def build_vocab(FLAGS):
 			max_corpus_chars = int(FLAGS.max_corpus_chars*(2**30)),
 			reserved_tokens = ['<EOS>'])
 	tokenizer.save_to_file(target_vocab_file)
-	tokenizer.save_to_file(target_vocab_file_short)
 	print('{}: the subwords tokenizer({}) is ready.'.format(time.asctime(), target_vocab_file))
 
 
@@ -92,10 +96,10 @@ if __name__ == '__main__':
 	flags.mark_flag_as_required('cmd')
 	flags.DEFINE_string('input_file', None, 'input dtitle file name for pre-process and build-vocab')
 	# params for pre-process
-	flags.DEFINE_boolean('filter_title', False, 'filter out content in <title> tag')
-	flags.DEFINE_boolean('filter_head', False, 'only keep content in <body> tag')
-	flags.DEFINE_boolean('ignore_noexactmatch', False, 'ignore examples when no exact-match title in body field')
-	flags.DEFINE_boolean('ignore_nofuzzymatch', False, 'ignore examples when no fuzzy-match title in body')
+	flags.DEFINE_boolean('remove_title', False, 'filter out content in <title> tag')
+	flags.DEFINE_boolean('remove_head', False, 'only keep content in <body> tag')
+	flags.DEFINE_boolean('check_exactmatch', False, 'filter out examples whose title doesn''t exact-match in html body')
+	flags.DEFINE_boolean('check_fuzzymatch', False, 'filter out examples whose title doesn''t fuzzy-match in html body')
 	# params for build-vocab
 	flags.DEFINE_string('vocab_file_prefix', None, 'the prefix of target vocab file for build-vocab')
 	flags.DEFINE_integer('target_vocab_size', 16384, 'target vocab size in build-vocab')
