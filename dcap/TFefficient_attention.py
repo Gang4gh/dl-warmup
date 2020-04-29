logger fix for storage shortage; padding_mask in LSH bucket calculation; positional encoding strategy; etc.

dcap/TFefficient_attention.py
@@ -58,7 +58,7 @@ class TFLSHAttention():
         self._allow_duplicate_attention = allow_duplicate_attention
         self._random_rotations_per_head = random_rotations_per_head
 
-    def hash_vectors(self, n_buckets, vecs):
+    def hash_vectors(self, n_buckets, vecs, padding_mask=None):
         batch_size = tf.shape(vecs)[0]
 
         # See https://arxiv.org/pdf/1509.02897.pdf
@@ -80,6 +80,9 @@ class TFLSHAttention():
         rotated_vecs = tf.einsum('btf,bfhi->bhti', vecs, random_rotations)
 
         rotated_vecs = tf.concat([rotated_vecs, -rotated_vecs], axis=-1)
+        if padding_mask is not None:
+          lastbucket = rotated_vecs[:,:,:,-1:] + tf.cast(padding_mask[:,None,:,None], tf.float32) * 1e9
+          rotated_vecs = tf.concat([rotated_vecs[:,:,:,:-1], lastbucket], axis=-1)
         buckets = tf.math.argmax(rotated_vecs, axis=-1)
         # buckets is now (batch_size, self.n_hashes, seqlen). Next we add offsets so that
         # bucket numbers from different hashing rounds don't overlap.
@@ -103,7 +106,7 @@ class TFLSHAttention():
         n_buckets = seqlen // self.bucket_size
         n_bins = n_buckets
 
-        buckets = self.hash_vectors(n_buckets, qk)
+        buckets = self.hash_vectors(n_buckets, qk, padding_mask)
         debug_print('buckets: ', buckets.shape)
         #buckets_0 = tf.reshape(buckets[0], (self.n_hashes, seqlen))
         #debug_print('buckets[0]: ', buckets_0[:,0:2])

dcap/attention_layer.py
@@ -22,7 +22,7 @@ from absl import flags
 import tensorflow as tf
 from official.nlp import bert_modeling as common_layer
 
-flags.DEFINE_enum('attention_padding_strategy', 'no-padding', ['classic', 'last-segment', 'no-padding'],
+flags.DEFINE_enum('attention_padding_strategy', 'default', ['default', 'nopadding'],
     help='padding strategy in LSH attention computation')
 flags.DEFINE_bool('allow_duplicated_attention', True,
     help='allow duplicated attention in LSH attention')
@@ -241,13 +241,8 @@ class LshSelfAttention(tf.keras.layers.Layer):
     #query *= depth ** -0.5
     #attention_output = calculate_full_attention(key, query, value, bias, training, self.attention_dropout)
 
-    if FLAGS.attention_padding_strategy == 'last-segment':
-      last_segment_start_pos = 128
-      padding_mask = tf.concat([tf.zeros([padding_mask.shape[0], last_segment_start_pos], tf.bool), padding_mask[:, last_segment_start_pos:]], axis=-1)
-    elif FLAGS.attention_padding_strategy == 'no-padding':
+    if FLAGS.attention_padding_strategy == 'nopadding':
       padding_mask = None
-    else:
-      pass # use input padding mask
 
     if self.use_full_attention_in_reformer:
       key = tf.math.l2_normalize(query, -1)

dcap/dtitle.py
@@ -29,6 +29,7 @@ from official.utils.flags import core as flags_core
 from official.utils.misc import keras_utils
 from official.utils.misc import distribution_utils
 import metrics
+import utils
 
 from data_dtitle.process_dtitle_data import dtitle_reader
 
@@ -140,7 +141,7 @@ class Seq2SeqTask():
 
     current_step = 0
     checkpoint = tf.train.Checkpoint(model=model)
-    ckpt_mgr = tf.train.CheckpointManager(checkpoint, flags_obj.model_dir, max_to_keep=3, keep_checkpoint_every_n_hours=12)
+    ckpt_mgr = tf.train.CheckpointManager(checkpoint, flags_obj.model_dir, max_to_keep=3, keep_checkpoint_every_n_hours=18)
     if ckpt_mgr.latest_checkpoint:
       #self._print_variables_and_exit(flags_obj.model_dir)
       model.fit([tf.ones([params["batch_size"], params['max_input_length']], tf.int32), tf.ones([params["batch_size"], params['max_target_length']], tf.int32)],
@@ -162,7 +163,7 @@ class Seq2SeqTask():
         initial_epoch=current_step // flags_obj.steps_between_evals,
         epochs=(flags_obj.train_steps-1) // flags_obj.steps_between_evals + 1,
         steps_per_epoch=min(flags_obj.steps_between_evals, flags_obj.train_steps - current_step),
-        callbacks=self._create_callbacks(flags_obj.model_dir, current_step, params, ckpt_mgr),
+        callbacks=self._create_callbacks(flags_obj.model_dir, current_step, flags_obj.steps_between_evals, params, ckpt_mgr),
         validation_data=val_ds,
         validation_steps=flags_obj.validation_example_count // params["batch_size"], # redundant but suppress one warining
         verbose=1)
@@ -191,7 +192,7 @@ class Seq2SeqTask():
     return self.tokenizer.decode(ids).replace(self._UNDERSCORE_REPLACEMENT, '_')
 
   def _trim_and_decode(self, ids, segment_count = 1, concatenate_segments = True):
-    """Trim EOS and PAD tokens from ids, and decode to return a string."""
+    """Trim EOS and PAD tokens from ids, and decode to a string."""
     ids = [id for id in list(ids) if id]
     indexes = [0]
     try:
@@ -202,8 +203,8 @@ class Seq2SeqTask():
       else:
         return [self._decode_and_fix(ids[indexes[i]:indexes[i+1]-1]) for i in range(len(indexes)-1)]
     except ValueError:  # No enough EOS found in input
-      return self._decode_and_fix(ids)
-
+      res = self._decode_and_fix(ids)
+      return res if concatenate_segments else [res]
 
   def _calculate_rouge_scores(self, summaries, references, max_length=None):
     # command to install pythonrouge: pip install git+https://github.com/tagucci/pythonrouge.git
@@ -237,7 +238,7 @@ class Seq2SeqTask():
       model.summary()
       self._load_model_weights(model)
 
-    #numpy.set_printoptions(threshold=sys.maxsize)
+    np.set_printoptions(threshold=sys.maxsize)
 
     ds = self._create_dataset(params['data_dir'], repeat=1, batch_size=1)
     if flags_obj.dev_mode:
@@ -249,7 +250,7 @@ class Seq2SeqTask():
     inputs, input_strings, targets, target_strings, preds, pred_strings, pred_scores, null_probs = [], [], [], [], [], [], [], []
     for ((inp, tar), _) in ds.unbatch():
       inputs.append(inp.numpy())
-      input_strings.append([re.sub(r'^<BOS#\d>', '', s) for s in self._trim_and_decode(inputs[-1], 3, concatenate_segments=False)])
+      input_strings.append([re.sub(r'<[EB]OS#\d>', '', s) for s in self._trim_and_decode(inputs[-1], concatenate_segments=False)])
       targets.append(tar.numpy())
       target_strings.append([self._trim_and_decode(targets[-1])])
     logging.info('load {} examples from {}'.format(len(targets), params['data_dir']))
@@ -306,25 +307,40 @@ class Seq2SeqTask():
         out_path = os.path.join(self.flags_obj.model_dir, 'prediction-compact-{}.txt'.format(timestamp))
       with open(out_path, 'w', encoding='utf8') as f:
         f.write('NormalizedUrl\tPredict\tNullProb\n')
-        for inp, pred, null_prob in zip(input_strings, pred_strings, null_probs):
-          f.write('{}\t{}\t{}\n'.format(inp[0], re.sub(r'[\t\r\n]+', ' ', html.unescape(pred)), null_prob)) # pred may contains '\n' after unescape
+        for inp, pred, tar, null_prob in zip(input_strings, pred_strings, target_strings, null_probs):
+          pred = re.sub(r'[\t\r\n]+', ' ', html.unescape(pred)) # pred may contains '\n' after unescape
+          if flags_obj.dev_mode:
+            f.write('{}\t{}\t{}\t{}\n'.format(inp[0], pred, tar[0], null_prob))
+          else:
+            f.write('{}\t{}\t{}\n'.format(inp[0], pred, null_prob))
+
       logging.info('write compact prediction to {}'.format(out_path))
 
 
-  def _create_callbacks(self, log_dir, init_steps, params, ckpt_mgr):
+  def _create_callbacks(self, log_dir, init_steps, steps_per_epoch, params, ckpt_mgr):
     """Creates a list of callbacks."""
     sfunc = optimizer.LearningRateFn(params["learning_rate"],
                                      params["hidden_size"],
                                      params["learning_rate_warmup_steps"])
+
+    def _save_checkpoint(epoch, logs):
+      if logs['steps'] % steps_per_epoch == 0:
+        try:
+          ckpt_mgr.save(checkpoint_number=epoch)
+        except:
+          logging.warning(f'save model failed due to an exception. continue without saving\n')
+      else:
+        logging.warning(f'not save model when training is interrupted. logs = {logs}\n')
+
     callbacks = []
     callbacks.append(optimizer.LearningRateScheduler(sfunc, init_steps))
-    callbacks.append(tf.keras.callbacks.LambdaCallback(on_epoch_end=lambda epoch, logs: ckpt_mgr.save()))
+    callbacks.append(tf.keras.callbacks.LambdaCallback(on_epoch_end=_save_checkpoint))
     if self.flags_obj.enable_tensorboard:
-      tensorboard_callback = tf.keras.callbacks.TensorBoard(
+      tensorboard_callback = utils.TensorBoardFix(start_step=init_steps,
           log_dir=log_dir, profile_batch=0, write_graph=False,
-          update_freq=self.flags_obj.steps_between_tensorboard_log * params['batch_size'])
+          update_freq=self.flags_obj.batches_between_tensorboard_log)
       callbacks.append(tensorboard_callback)
-    callbacks.append(tf.keras.callbacks.CSVLogger('{}/history.step-{}.log'.format(log_dir, init_steps)))
+    callbacks.append(utils.CSVLoggerFix(f'{log_dir}/history.step-{init_steps}.log'))
     return callbacks
 
   def _load_model_weights(self, model):
@@ -441,7 +457,7 @@ class Seq2SeqTask():
     description = self._create_description_from_names(datasetv3_schema)
 
     input_schema, target_schema = (s.strip() for s in self.flags_obj.training_schema.split('=>'))
-    names_limits = [(v[0], int(v[1]) if len(v) > 2 else max_input_length) for v in [col.split(':') for col in input_schema.split(',')]]
+    names_limits = [(v[0], int(v[1]) if len(v) > 1 else max_input_length) for v in [col.split(':') for col in input_schema.split(',')]]
 
     def _tf_parse_and_truncate_v3(proto):
       def _cast_and_concat(*values):
@@ -452,9 +468,26 @@ class Seq2SeqTask():
       target = _cast_and_concat(ex[target_schema], [eos])
       return inputs[:max_input_length], target
 
+    def _filter_fn(inp, tar):
+      return tf.size(tar) <= max_target_length
+
+    #r = tf.random.uniform(shape=[])
+    #positive, negative = tf.Variable(0, dtype=tf.int64), tf.Variable(0, dtype=tf.int64)
+    #def _filter_fn_v2(inp, tar):
+    #  if tf.size(tar) > max_target_length: return False
+    #  if tf.size(tar) > 1:
+    #    positive.assign_add(1)
+    #    return True
+    #  else:
+    #    if negative / (positive + negative) < 0.05:
+    #      negative.assign_add(1)
+    #      return True
+    #    else:
+    #      return False
+
     ds = tf.data.TFRecordDataset(data_file, compression_type='GZIP' if data_file.endswith('.gz') else None)
     ds = ds.map(_tf_parse_and_truncate_v3, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds = ds.filter(lambda _, target: tf.size(target) <= max_target_length)
+    ds = ds.filter(_filter_fn)
     ds = ds.padded_batch(batch_size, padded_shapes=([max_input_length], [max_target_length]), drop_remainder=True)
     return ds
 
@@ -535,8 +568,10 @@ def test_read_and_dump_datasets(task):
         for batch, ((inp, tar), _) in enumerate(ds):
           if batch == batch_count: break
           for (index, (inp1, tar1)) in enumerate(zip(inp, tar)):
-            htmlbody = decode_fn(inp1.numpy(), 1, concatenate_segments=False)
+            htmlbody = decode_fn(inp1.numpy(), concatenate_segments=False)
             title = decode_fn(tar1.numpy(), concatenate_segments=False)
+            inp1 = [i for i in inp1.numpy() if i]
+            tar1 = [i for i in tar1.numpy() if i]
             ow.write('{}.{}:\ninp = {}\ntar = {}\ninp_str = {}\ntar_str = {}\n'.format(batch, index, inp1, tar1, htmlbody, title))
       logging.info(f'end of round {r}, --- {int(time.time() - start_time)} seconds ---')
 
@@ -550,8 +585,25 @@ def test_read_and_dump_datasets(task):
     _dump_to_file(f'out-dtitle-tokenized-gz-{idx}.batch{bc}', task.params['data_dir'], batch_count=bc, decode_fn=task._trim_and_decode, repeat=repeat_times)
 
 
+def count_token_id_freq(task):
+  from collections import Counter
+  ds = task._create_dataset(task.params['data_dir'], repeat=1)
+  freq = Counter()
+  for batch, ((inp, tar), _) in enumerate(ds):
+    if batch % (1024) == 0:
+      print(f'batch = {batch//1024}K')
+    for inp1, tar1 in zip(inp, tar):
+      #freq.update(inp1.numpy())
+      freq.update(tar1.numpy())
+  print(f'batch = {batch}')
+  with open('token_id_freq.txt', 'w') as ow:
+    for key, value in freq.items():
+      ow.write(f'{key}\t{value}\n')
+
+
 def test(task):
-  test_read_and_dump_datasets(task)
+  #test_read_and_dump_datasets(task)
+  count_token_id_freq(task)
 
 def main(_):
   flags_obj = flags.FLAGS

dcap/reformer.py
@@ -4,15 +4,21 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from absl import flags
 import tensorflow as tf
 
 import utils as model_utils
-from official.transformer.utils.tokenizer import EOS_ID
 import attention_layer
 from official.transformer.v2 import beam_search
 from official.transformer.v2 import embedding_layer
 from official.transformer.v2 import ffn_layer
 import metrics
+
+
+flags.DEFINE_enum('positional_encoding_strategy', 'default',
+                  ['default', 'concat-4', 'concat-8', 'concat-16'],
+                  help='different strategy for positional encoding')
+FLAGS = flags.FLAGS
 
 
 def create_model(params, mode):
@@ -42,9 +48,10 @@ def create_model(params, mode):
       outputs, scores = ret["outputs"], ret["scores"]
       return tf.keras.Model([inputs, targets], [outputs, scores, logits])
 
-#IMPORTANT: THIS IS NOT A COMPLETE AND WORKING REFORMER implementation
+
 class Reformer(tf.keras.Model):
   """Reformer model with Keras.
+  IMPORTANT: THIS IS NOT A COMPLETE AND WORKING REFORMER implementation
 
   The Reformer model consists of an encoder and decoder. The input is an int
   sequence (or a batch of sequences). The encoder produces a continuous
@@ -61,8 +68,11 @@ class Reformer(tf.keras.Model):
     """
     super(Reformer, self).__init__(name=name)
     self.params = params
+    self.positional_encoding_strategy = pes = FLAGS.positional_encoding_strategy
+    self.positional_encoding_concat_dimension = int(pes[7:]) if pes.startswith('concat-') else 0
     self.embedding_softmax_layer = embedding_layer.EmbeddingSharedWeights(
-        params["vocab_size"], params["hidden_size"])
+        params["vocab_size"], params["hidden_size"] - self.positional_encoding_concat_dimension)
+
     self.encoder_stack = EncoderStack(params)
     self.decoder_stack = DecoderStack(params)
 
@@ -118,6 +128,21 @@ class Reformer(tf.keras.Model):
         logits = self.decode(targets, encoder_outputs, attention_bias, training)
         return logits
 
+  def _apply_positional_encoding(self, inputs):
+    with tf.name_scope("pos_encoding"):
+      inputs_shape = tf.shape(inputs)
+      batch_size, length = inputs_shape[0], inputs_shape[1]
+      if self.positional_encoding_strategy == 'default':
+        pos_encoding = model_utils.get_position_encoding(length, self.params["hidden_size"])
+        pos_encoding = tf.cast(pos_encoding, self.params["dtype"])
+        pos_encoded_inputs = inputs + pos_encoding
+      else:  # concat-n
+        pos_dim = self.positional_encoding_concat_dimension
+        pos_encoding = model_utils.get_position_encoding(length, pos_dim)
+        pos_encoding = tf.broadcast_to(pos_encoding, [batch_size, length, pos_dim])
+        pos_encoded_inputs = tf.concat([inputs, pos_encoding], axis=-1)
+    return pos_encoded_inputs
+
   def encode(self, inputs, training):
     """Generate continuous representation for inputs.
 
@@ -133,14 +158,9 @@ class Reformer(tf.keras.Model):
       # applying dropout.
       embedded_inputs = self.embedding_softmax_layer(inputs)
       embedded_inputs = tf.cast(embedded_inputs, self.params["dtype"])
-      padding_mask = tf.cast(tf.equal(inputs, 0), tf.bool) # assume inputs is padded by '0'
+      padding_mask = tf.cast(tf.equal(inputs, 0), tf.bool)  # assume inputs is padded by '0'
 
-      with tf.name_scope("add_pos_encoding"):
-        length = tf.shape(embedded_inputs)[1]
-        pos_encoding = model_utils.get_position_encoding(
-            length, self.params["hidden_size"])
-        pos_encoding = tf.cast(pos_encoding, self.params["dtype"])
-        encoder_inputs = embedded_inputs + pos_encoding
+      encoder_inputs = self._apply_positional_encoding(embedded_inputs)
 
       if training:
         encoder_inputs = tf.nn.dropout(
@@ -173,17 +193,15 @@ class Reformer(tf.keras.Model):
         # Shift targets to the right, and remove the last element
         decoder_inputs = tf.pad(decoder_inputs,
                                 [[0, 0], [1, 0], [0, 0]])[:, :-1, :]
-      with tf.name_scope("add_pos_encoding"):
-        length = tf.shape(decoder_inputs)[1]
-        pos_encoding = model_utils.get_position_encoding(
-            length, self.params["hidden_size"])
-        pos_encoding = tf.cast(pos_encoding, self.params["dtype"])
-        decoder_inputs += pos_encoding
+
+      decoder_inputs = self._apply_positional_encoding(decoder_inputs)
+
       if training:
         decoder_inputs = tf.nn.dropout(
             decoder_inputs, rate=self.params["layer_postprocess_dropout"])
 
       # Run values
+      length = tf.shape(decoder_inputs)[1]
       decoder_self_attention_bias = model_utils.get_decoder_self_attention_bias(
           length, dtype=self.params["dtype"])
       outputs = self.decoder_stack(
@@ -192,6 +210,8 @@ class Reformer(tf.keras.Model):
           decoder_self_attention_bias,
           attention_bias,
           training=training)
+      if self.positional_encoding_concat_dimension > 0:
+        outputs = outputs[:, :, :-self.positional_encoding_concat_dimension]
       logits = self.embedding_softmax_layer(outputs, mode="linear")
       logits = tf.cast(logits, tf.float32)
       return logits
@@ -200,7 +220,7 @@ class Reformer(tf.keras.Model):
     """Returns a decoding function that calculates logits of the next tokens."""
 
     timing_signal = model_utils.get_position_encoding(
-        max_decode_length + 1, self.params["hidden_size"])
+        max_decode_length, self.params["hidden_size"])
     timing_signal = tf.cast(timing_signal, self.params["dtype"])
     decoder_self_attention_bias = model_utils.get_decoder_self_attention_bias(
         max_decode_length, dtype=self.params["dtype"])
@@ -259,11 +279,11 @@ class Reformer(tf.keras.Model):
     """Return predicted sequence."""
     if self.params["padded_decode"]:
       batch_size = encoder_outputs.shape.as_list()[0]
-      input_length = encoder_outputs.shape.as_list()[1]
+      #input_length = encoder_outputs.shape.as_list()[1]
     else:
       batch_size = tf.shape(encoder_outputs)[0]
-      input_length = tf.shape(encoder_outputs)[1]
-    max_decode_length = 48#input_length + self.params["extra_decode_length"]
+      #input_length = tf.shape(encoder_outputs)[1]
+    max_decode_length = 48  # input_length + self.params["extra_decode_length"]
     encoder_decoder_attention_bias = tf.cast(encoder_decoder_attention_bias,
                                              self.params["dtype"])
 
@@ -308,7 +328,7 @@ class Reformer(tf.keras.Model):
         beam_size=self.params["beam_size"],
         alpha=self.params["alpha"],
         max_decode_length=max_decode_length,
-        eos_id=self.params["vocab_size"] + 1, #EOS_ID,
+        eos_id=self.params["vocab_size"] + 1,  # EOS_ID,
         padded_decode=self.params["padded_decode"],
         dtype=self.params["dtype"])
 

dcap/utils.py
@@ -19,8 +19,8 @@ from __future__ import division
 from __future__ import print_function
 
 import math
-
 import numpy as np
+from absl import logging
 import tensorflow as tf
 
 # Very low numbers to represent -infinity. We do not actually use -Inf, since we
@@ -29,8 +29,10 @@ _NEG_INF_FP32 = -1e9
 _NEG_INF_FP16 = np.finfo(np.float16).min
 
 
-def get_position_encoding(
-    length, hidden_size, min_timescale=1.0, max_timescale=1.0e4):
+def get_position_encoding(length,
+                          hidden_size,
+                          min_timescale=1.0,
+                          max_timescale=1.0e4):
   """Return positional encoding.
 
   Calculates the position encoding as a mix of sine and cosine functions with
@@ -121,3 +123,42 @@ def get_padding_bias(x, padding_value=0, dtype=tf.float32):
     attention_bias = tf.expand_dims(
         tf.expand_dims(attention_bias, axis=1), axis=1)
   return attention_bias
+
+
+class TensorBoardFix(tf.keras.callbacks.TensorBoard):
+    """Build-in TensorBoard can't resume training_step, so fix it"""
+    def __init__(self, start_step=0, *args, **kwargs):
+        super(TensorBoardFix, self).__init__(*args, **kwargs)
+        self.start_step = start_step
+        self.buffer = []
+
+    def on_train_begin(self, logs=None):
+        super(TensorBoardFix, self).on_train_begin(logs)
+        self._total_batches_seen[self._train_run_name] = self.start_step
+        del self.start_step
+
+    def on_train_batch_end(self, batch, logs=None):
+        super(TensorBoardFix, self).on_train_batch_end(batch+1, logs)
+
+    def _log_metrics(self, logs, prefix, step):
+        self.buffer.extend([logs, prefix, step])
+        try:
+            while self.buffer:
+                super(TensorBoardFix, self)._log_metrics(*self.buffer[:3])
+                self.buffer = self.buffer[3:]
+        except:
+            logging.warning(f'write buffer to tensorboard failed, logs={logs}, prefix={prefix}, step={step}\n')
+
+class CSVLoggerFix(tf.keras.callbacks.CSVLogger):
+    def __init__(self, *args, **kwargs):
+        super(CSVLoggerFix, self).__init__(*args, **kwargs)
+        self.buffer = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.buffer.extend([epoch, logs])
+        try:
+            while self.buffer:
+                super(CSVLoggerFix, self).on_epoch_end(*self.buffer[:2])
+                self.buffer = self.buffer[2:]
+        except:
+            logging.warning(f'write buffer to csv failed, epoch={epoch}, logs={logs}\n')